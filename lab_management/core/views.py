55b229a1e388324d44e59d801a245351036a6fe0
kennethmyrond <kenneth_uy@dlsu.edu.ph>
2024-09-18 14:57:39 +0800

    new start

---
+++ lab_management/core/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm
+from .models import Laboratory, Module
+
+
+def userlogin(request):
+    return render(request,"user_login.html")
+
+@login_required(login_url='/login')
+def home(request):
+    return render(request,"home.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/login")
+
+def inventory_view(request):
+    return render(request, 'mod_inventory/inventory.html')
+
+def borrowing_view(request):
+    return render(request, 'mod_borrowing/borrowing.html')
+
+def clearance_view(request):
+    return render(request, 'mod_clearance/clearance.html')
+
+def lab_reservation_view(request):
+    return render(request, 'mod_labRes/lab_reservation.html')
+
+def reports_view(request):
+    return render(request, 'mod_reports/reports.html')
+
+def user_settings_view(request):
+    return render(request, 'user_settings.html')
+
+
+
+# superuser stuff
+
+def superuser_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('superuser_setup')
+    else:
+        form = LoginForm()
+    return render(request, 'superuser/superuser_login.html', {'form': form})
+
+def superuser_logout(request):
+    logout(request)
+    return redirect("/login/superuser")
+
+@login_required(login_url='/login/superuser')
+def superuser_setup(request):
+    if not request.user.is_superuser:
+        return redirect('userlogin')
+
+    labs = Laboratory.objects.all()
+    modules = Module.objects.all()
+    return render(request, 'superuser/superuser_setup.html', {'labs': labs, 'modules': modules})
+
+
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import LaboratoryForm
+
+@login_required(login_url='/login/superuser')
+def add_laboratory(request):
+    if not request.user.is_superuser:
+        return redirect('login')
+
+    if request.method == 'POST':
+        form = LaboratoryForm(request.POST)
+        if form.is_valid():
+            laboratory = form.save(commit=False)
+            # Save the laboratory instance first
+            laboratory.save()
+            # Then save the selected modules
+            form.save_m2m()
+            return redirect('superuser_setup')
+    else:
+        form = LaboratoryForm()
+
+    return render(request, 'superuser/add_laboratory.html', {'form': form})
+
