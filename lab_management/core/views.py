311f4eb58bf934fdfdce1854a7c95d0f1641630e
dominique conde <dominique_conde@dlsu.edu.ph>
2024-10-06 15:35:51 +0800

    Lab Tech Prebooking requests

--- lab_management/core/views.py
+++ lab_management/core/views.py
@@ -1241,6 +1241,9 @@ def borrowing_labcoord_borrowconfig(request):
         'questions': lab.get_questions()  # Get the questions to display them
     })
 
+def borrowing_labtech_prebookrequests(request):
+    return render(request, 'mod_borrowing/borrowing_labtech_prebookrequests.html')
+
 #CLEARANCE
 def clearance_view(request):
     return render(request, 'mod_clearance/clearance.html')
