8be0881604cdf28c99fae8888d9fcbb73fa763d0
merylle-salvador-fgi <merylle.salvador@focusglobalinc.com>
2024-10-08 13:13:09 +0800

    made clearance student side

--- lab_management/core/urls.py
+++ lab_management/core/urls.py
@@ -50,6 +50,8 @@ urlpatterns = [
     path('borrowing/labtech/detailedprebookrequests', views.borrowing_labtech_detailedprebookrequests, name='borrowing_labtech_detailedprebookrequests'),
     
     path('clearance/', views.clearance_view, name='clearance'),
+    path('clearance/student/view_clearance', views.clearance_student_viewClearance, name='clearance_student_viewClearance'),
+        path('clearance/student/view_clearance_detailed', views.clearance_student_viewClearanceDetailed, name='clearance_student_viewClearanceDetailed'),
     path('lab-reservation/', views.lab_reservation_view, name='lab_reservation'),
     path('reports/', views.reports_view, name='reports'),
     path('user-settings/', views.user_settings_view, name='user_settings'),
