258f52520cd0b0ea66d3fdebfe8fca1fbbeb462a
root <root@ALTDSI-R06-LabMas.dlsu.edu.ph>
2025-02-23 17:37:57 +0000

    ccscloud mysql connection fixed

--- lab_management/lab_management/settings.py
+++ lab_management/lab_management/settings.py
@@ -150,26 +150,26 @@ WSGI_APPLICATION = 'lab_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'dblabmas_v2',
-#         'USER': 'root',
-#         'PASSWORD': 'DLSU1234',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
-#     }
-# }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'dblabmas_v2',
+         'USER': 'django_user',
+         'PASSWORD': 'Dlsu1234!',
+         'HOST': '127.0.0.1',
+         'PORT': '3306',
+         'OPTIONS': {
+             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+         }
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
